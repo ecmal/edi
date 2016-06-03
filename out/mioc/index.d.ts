178a4c6e93420229752e976f0859d7a4430a8747
Sergey Mamyan <sergey.mamyan@gmail.com>
2016-06-04 02:15:49 +0400

    Remove out folder

--- out/mioc/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from "./injector";
-export * from "./injectable";
-export * from "./inject";
--- out/mioc/injectable.d.ts
+++
@@ -1,8 +0,0 @@
-import { Decorator } from "runtime/decorators";
-import { Member } from "runtime/reflect/class";
-export default Injectable;
-export declare class Injectable extends Decorator {
-    options: any;
-    constructor(options: any);
-    decorate(member: Member): void;
-}
